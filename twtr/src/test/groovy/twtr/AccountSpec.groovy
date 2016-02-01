f489a150732f6ed0bbc8eac57816056c0b2a1a84
donaldsawyer <donald.sawyer@gmail.com>
2016-01-31 23:51:27 -0600

    Added tests for handle and email address
    I could probably refactor them to use the where: section for these.

--- twtr/src/test/groovy/twtr/AccountSpec.groovy
+++ twtr/src/test/groovy/twtr/AccountSpec.groovy
@@ -14,6 +14,11 @@ import spock.lang.Specification
 @TestMixin(GrailsUnitTestMixin)
 class AccountSpec extends Specification {
 
+    final String goodHandle = "@scsu-huskies"
+    final String goodEmail  = "testemail@test.com"
+    final String goodPassword = "abc123ABC"
+    final String goodDisplayName = "SCSU Huskies"
+
     def setup() {
 
     }
@@ -22,38 +27,131 @@ class AccountSpec extends Specification {
 
     }
 
+    // HAPPY PATH TESTS //
     void "account field values follow constraints for new account :-)"() {
         setup:
 
         when:
-        def sus = new Account(handle: "@scsu-huskies", emailAddress: "testemail@test.com",
-                              password: "abc123ABC", displayName: "SCSU Huskies")
+        def sus = new Account(handle: goodHandle, emailAddress: goodEmail,
+                              password: goodPassword, displayName: goodDisplayName)
         then:
         sus.validate()
     }
+    // END OF HAPPY PATH TESTS //
 
-    void "account with invalid password format :-("() {
-        when:
-        def sus = new Account(handle: "@scsu-huskies", emailAddress: "testemail@test.com",
-                password: "abc123abc", displayName: "SCSU Huskies")
-
-        then:
-        !sus.validate()
-    }
-
+    // HANDLE TESTS //
     void "accounts with same handle cannot be created"() {
         setup:
-        def account1 = new Account(handle: "@scsu-huskies", emailAddress: "testemail@test.com",
-                password: "abc123ABC", displayName: "SCSU Huskies")
+        def account1 = new Account(handle: goodHandle, emailAddress: goodEmail,
+                password: goodPassword, displayName: goodDisplayName)
         account1.save(flush: true)
 
         when:
-        def sus = new Account(handle: "@scsu-huskies", emailAddress: "testemail2@test.com",
-                password: "abc123ABC", displayName: "SCSU Huskies")
+        def sus = new Account(handle: goodHandle, emailAddress: "testemail2@test.com",
+                password: goodPassword, displayName: goodDisplayName)
 
         then:
         !sus.validate()
     }
+
+    void "accounts with an empty handle are invalid"() {
+        when:
+        def sus = new Account(handle: "", emailAddress: goodEmail, password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts missing a handle are invalid"() {
+        when:
+        def sus = new Account(emailAddress: goodEmail, password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+    // END OF HANDLE TESTS //
+
+    // EMAIL ADDRESS TESTS //
+    void "accounts missing an emailAddress are invalid"() {
+        when:
+        def sus = new Account(handle: goodHandle, password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with an empty emailAddress are invalid"() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: "", password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with whitespace for an emailAddress are invalid"() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: "    ", password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with an invalid email format are invalid - no @"() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: "umn.edu",
+                              password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with an invalid email format are invalid - @@"() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: "sawy0059@@umn.edu",
+                              password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with an invalid email format are invalid - comma instead of period"() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: "sawy0059@umn,edu",
+                              password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+
+    void "accounts with the same email address cannot be used"() {
+        setup:
+        Account account1 = new Account(handle: goodHandle, emailAddress: goodEmail,
+                                       password: goodPassword, displayName: goodDisplayName)
+        account1.save(flush: true)
+
+        when:
+        Account sus = new Account(handle: goodHandle+"2", emailAddress: goodEmail,
+                                  password: goodPassword, displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+    // END OF EMAIL ADDRESS TESTS //
+
+    // PASSWORD TESTS //
+    void "account with invalid password format :-("() {
+        when:
+        def sus = new Account(handle: goodHandle, emailAddress: goodEmail,
+                              password: "abc123abc", displayName: goodDisplayName)
+
+        then:
+        !sus.validate()
+    }
+    // END OF PASSWORD TESTS //
+
+    // DISPLAY NAME TESTS //
+
+    // END OF DISPLAY NAME TESTS //
 
     /* UNIT TESTS WE STILL NEED
     handle
