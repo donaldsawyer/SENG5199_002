fa9b0109934a281235c4b70ed04e17080fcaa5a0
donaldsawyer <donald.sawyer@gmail.com>
2016-02-20 12:08:40 -0600

    Cleaner bootstrapping

--- twtr/grails-app/init/BootStrap.groovy
+++ twtr/grails-app/init/BootStrap.groovy
@@ -7,10 +7,10 @@ class BootStrap {
         String pwd = "abcABC123!@#"
         String edu = "@scsu.edu"
 
-        Account a1 = new Account(handle: "luluwang", password: pwd, emailAddress: "lulu.wang"+edu, displayName: "Lulu Wang").save()
-        Account a2 = new Account(handle: "donaldsawyer", password: pwd, emailAddress: "donald.sawyer"+edu, displayName: "Donald Sawyer").save()
-        Account s3 = new Account(handle: "mikecalvo", password: pwd, emailAddress: "mike.calvo"+edu, displayName: "Mike Calvo").save()
-        Account s4 = new Account(handle: "blizzard", password: pwd, emailAddress: "blizzard"+edu, displayName: "Blizzard").save()
+        new Account(handle: "luluwang", password: pwd, emailAddress: "lulu.wang"+edu, displayName: "Lulu Wang").save()
+        new Account(handle: "donaldsawyer", password: pwd, emailAddress: "donald.sawyer"+edu, displayName: "Donald Sawyer").save()
+        new Account(handle: "mikecalvo", password: pwd, emailAddress: "mike.calvo"+edu, displayName: "Mike Calvo").save()
+        new Account(handle: "blizzard", password: pwd, emailAddress: "blizzard"+edu, displayName: "Blizzard").save()
     }
     def destroy = {
     }
