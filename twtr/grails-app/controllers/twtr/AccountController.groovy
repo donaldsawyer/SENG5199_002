5d61b6335a8e4f6191694452e6f1e865e237f4fd
donaldsawyer <donald.sawyer@gmail.com>
2016-02-20 15:29:42 -0600

    Set up the original account controller functionality

--- twtr/grails-app/controllers/twtr/AccountController.groovy
+++ twtr/grails-app/controllers/twtr/AccountController.groovy
@@ -1,11 +1,38 @@
 package twtr
 
 class AccountController {
+    
+    def get() {
+        def theAccount = Account.get(params.id)
+        if(!theAccount) {
+            response.sendError(404)
+        }
+        else {
+            render(contentType: "application/json") {
+                account (id: theAccount.id,
+                        handle: theAccount.handle,
+                        password: theAccount.password,
+                        displayName: theAccount.displayName)
+            }
+        }
+    }
 
-    static scaffold = Account
+    def index() {
+        saveIfNotExists(goodAccount1)
 
-//    def index() {
-//        Account account = new Account()
-//        account.displayName.vali
-//    }
+        def allAccounts = Account.list()
+        render (contentType: "application/json") {
+            accounts = array {
+                for (a in allAccounts) {
+                    account (id: a.id,
+                            handle: a.handle)
+                }
+            }
+        }
+    }
+
+    def createAccount() {
+        Account goodAccount = new Account(handle: request.JSON.handle, emailAddress: request.JSON.email,
+                password: request.JSON.password, displayName: request.JSON.displayName)
+    }
 }
