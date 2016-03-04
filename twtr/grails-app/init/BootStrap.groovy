6c183f37771b8bc6c5d8b7b52d2f99ddb9bc1449
Donald Sawyer <donald.sawyer@target.com>
2016-03-03 20:11:16 -0600

    updates for account controller

--- twtr/grails-app/init/BootStrap.groovy
+++ twtr/grails-app/init/BootStrap.groovy
@@ -1,4 +1,5 @@
 import twtr.Account
+import twtr.Message
 
 class BootStrap {
 
@@ -11,6 +12,11 @@ class BootStrap {
         Account acct2 = new Account(handle: "donaldsawyer", password: pwd, emailAddress: "donald.sawyer"+edu, displayName: "Donald Sawyer").save()
         Account acct3 = new Account(handle: "mikecalvo", password: pwd, emailAddress: "mike.calvo"+edu, displayName: "Mike Calvo").save()
         Account acct4 = new Account(handle: "blizzard", password: pwd, emailAddress: "blizzard"+edu, displayName: "Blizzard").save()
+
+        acct1.addToMessages(messageText: "Message 1_1");
+        acct1.addToMessages(messageText: "Message 1_2").save()
+
+        acct4.addToMessages(messageText: "Message 4_1").save()
 
     }
     def destroy = {
