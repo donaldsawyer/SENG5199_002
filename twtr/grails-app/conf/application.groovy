7fa339f64cd676fae7f793d5adc13fae42f6dc99
LuluW <lulu@Lulus-MacBook-Pro.local>
2016-03-28 22:16:02 -0500

    Test cases passed for User domain.
    Now need to update User domain to Account domain.

--- twtr/grails-app/conf/application.groovy
+++ twtr/grails-app/conf/application.groovy
@@ -9,13 +9,13 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 ]
 
 grails.plugin.springsecurity.rest.token.storage.useGorm = true
-grails.plugin.springsecurity.rest.token.storage.gorm.tokenDomainClassName = 'grails.security.AuthenticationToken'
+grails.plugin.springsecurity.rest.token.storage.gorm.tokenDomainClassName = 'security.AuthenticationToken'
 grails.plugin.springsecurity.rest.token.validation.useBearerToken = false
 grails.plugin.springsecurity.rest.token.validation.headerName = 'X-Auth-Token'
 
-grails.plugin.springsecurity.userLookup.userDomainClassName = 'grails.security.User'
-grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'grails.security.UserRole'
-grails.plugin.springsecurity.authority.className = 'grails.security.Role'
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'security.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'security.UserRole'
+grails.plugin.springsecurity.authority.className = 'security.Role'
 
 grails.plugin.springsecurity.securityConfigType = 'InterceptUrlMap'
 grails.plugin.springsecurity.interceptUrlMap = [
