8db2162f7ad8d80a6c3f86502147f0a2f14add06
Donald Sawyer <donald.sawyer@target.com>
2016-04-02 16:41:59 -0500

    Working on first login test

---
+++ twtr/src/integration-test/groovy/twtr/webappTests/AdminLoginOk.groovy
@@ -0,0 +1,42 @@
+package twtr.webappTests
+
+import grails.test.mixin.integration.Integration
+import spock.lang.Stepwise
+import twtr.TwtrFunctionalTestBase
+
+@Integration
+@Stepwise
+public class AdminLoginOk extends TwtrFunctionalTestBase
+{
+
+    def 'login button logs in'() {
+        when:
+        go '/'
+        $("#login-form input[id=loginHandle]").value("admin")
+        $("#login-form input[id=loginPassword]").value("ABCDr00t!")
+        $("#login-form input[type=submit]").click()
+
+        then:
+        $("#auth-token").text() != "{}"
+
+    }
+//
+//import geb.spock.GebSpec
+//import grails.test.mixin.integration.Integration
+//
+//@Integration
+//class WelcomePageFunctionalSpec extends GebSpec {
+//
+//    def 'welcome page displays welcome message'() {
+//        when:
+//        go '/'
+//
+//        then: 'Static welcome displayed properly'
+//        $('h1').first().text() == 'Hello World'
+//
+//        and: 'Angular generated test displayed properly'
+//        $('h2').first().text() == 'Hello Stranger'
+//    }
+//
+//}
+}
