30c5108fee7b5da2a05f699e2353b759825913db
Donald Sawyer <donald.sawyer@target.com>
2016-04-02 13:15:09 -0500

    Fighting with angular routing still.

---
+++ twtr/grails-app/assets/javascripts/app/routes/twtr.js
@@ -0,0 +1,21 @@
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'app/home.htm',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: 'twtr/partials/about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl: 'twtr/partials/contact.html',
+            controller: 'contactController'
+        })
+        .when('/attendee/:action?/:id?', {
+            templateUrl: 'twtr/partials/attendee.html'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
