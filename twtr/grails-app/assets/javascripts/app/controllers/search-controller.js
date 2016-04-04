34fe7ca95628a1d203cc3b23759a8420a17dd699
LuluW <lulu@Lulus-MacBook-Pro.local>
2016-04-04 00:34:08 -0500

    S1, S2, S3 completed

---
+++ twtr/grails-app/assets/javascripts/app/controllers/search-controller.js
@@ -0,0 +1,39 @@
+angular.module('app').controller('searchController', function ($scope, $location, $http, authService, accountService) {
+
+    $scope.message = "Search tweets";
+
+    $scope.auth = {};
+    $scope.auth.token = authService.getToken();
+    $scope.auth.username = authService.getUsername();
+    $scope.account = accountService.getAccount();
+
+    $scope.doSearch = function () {
+        $http.get('/message/search',
+            {
+                params: {handle: $scope.search.tweeterHandle, text: $scope.search.tweetMessage},
+                headers: {'X-Auth-Token': authService.getToken().toString()}
+            })
+            .success(function (data) {
+                accountService.setAccount(data);
+                $scope.account = accountService.getAccount();
+            })
+            .error(function (error) {
+                $scope.notFound = "No tweets found"
+            })
+            .finally(function () {
+                $scope.account = accountService.getAccount();
+                $location.path("/search");
+            })
+    };
+
+    $scope.goToUserDetails = function() {
+        //TBD - navigate to user details page based on the handle
+        if($scope.testMessage == "Test")
+        {
+            $scope.testMessage = "Toggle Test";
+        }
+        else {
+            $scope.testMessage = "Test";
+        }
+    };
+});
