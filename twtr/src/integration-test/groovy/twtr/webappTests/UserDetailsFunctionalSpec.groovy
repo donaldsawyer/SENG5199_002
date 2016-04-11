ec030714c5a2f38e37f39cc6db72f2467ccfdb9c
Donald Sawyer <donald.sawyer@target.com>
2016-04-10 23:00:46 -0500

    added user detail follow test

--- twtr/src/integration-test/groovy/twtr/webappTests/UserDetailsFunctionalSpec.groovy
+++ twtr/src/integration-test/groovy/twtr/webappTests/UserDetailsFunctionalSpec.groovy
@@ -22,12 +22,11 @@ public class UserDetailsFunctionalSpec extends TwtrFunctionalTestBase
         when: 'user is navigated to user details page'
         go '/#/userDetail'
         waitFor (5, 0.1) {$('form').find("div", id: "feed-page-status").text() == "Page load complete"}
+        sleep(1000)
 
         then: "user's name, email, and tweets are displayed"
         $('#user-name').text() == "User's Name: display name"
         $('#user-email').text() == "User's Email: abac@abc.com"
-//        $('form').find("input", id:"userDisplayName").text() == "display name"
-//        $('form').find("input", id:"userEmail").text() == "abac@abc.com"
         $('form').find("button", id: "save-account").enabled
         $('form').find("td", id:"td-feed-content").allElements().size() == 10
         $('form').find("td", id:"td-feed-content").allElements()[0].getText() == "Admin Written Message #20"
@@ -44,15 +43,6 @@ public class UserDetailsFunctionalSpec extends TwtrFunctionalTestBase
         then: "user's name and email are updated"
         $('#user-name').text() == "User's Name: updated Name"
         $('#user-email').text() == "User's Email: updatedemail@email.com"
-    }
-
-    def 'user admin starts to follow mikecalvo'(){
-        when: "user mikecalvo's user detail page is displayed"
-        //this requires the debug pane to be available
-
-        then: "admin can follow mikecalvo"
-        true
-
     }
 
 }
