7b669bf97a94d3060d5064ecb8ab77c385ae6f37
donaldsawyer <donald.sawyer@gmail.com>
2016-05-05 07:40:16 -0500

    Revert "Calvo assignment3 feedback"

--- twtr/src/integration-test/groovy/twtr/webappTests/UserLoginErrorsSpec.groovy
+++ twtr/src/integration-test/groovy/twtr/webappTests/UserLoginErrorsSpec.groovy
@@ -11,15 +11,15 @@ public class UserLoginErrorsSpec extends TwtrFunctionalTestBase {
     def 'Admin logs in with the Login button with incorrect password'() {
         when: 'admin logs in with invalid password'
         go '/'
-        $('#loginHandle').value('admin')
-        $('#loginPassword').value('BadPassword')
-        $('#do-login').click()
-        waitFor(10, 0.1) { $('#page-status').text() == 'Page load complete' }
+        $("#login-form input[id=loginHandle]").value("admin")
+        $("#login-form input[id=loginPassword]").value("BadPassword")
+        $("#login-form button[id=do-login]").click()
+        waitFor(10, 0.1) { $('form').find("div", id: "page-status").text() == "Page load complete" }
 
         then: 'system displays error message to user'
-        $('#error-login').text() == 'Username and Password do not match a valid user.'
-        !$('#logged-in-message').text()
-        !$('#logout').text()
+        $('form').find("div", id: "error-login").text() == "Username and Password do not match a valid user."
+        !$('form').find("h2", id: "logged-in-message").text()
+        !$('form').find("button", id: "logout").text()
     }
 
     def 'mikecalvo logs in with the Login button with incorrect password'() {
---
+++ twtr/src/integration-test/groovy/twtr/webappTests/WelcomePageFunctionalSpec.groovy
@@ -0,0 +1,20 @@
+//package twtr.webappTests
+//
+//import geb.spock.GebSpec
+//import grails.test.mixin.integration.Integration
+//
+//@Integration
+//class WelcomePageFunctionalSpec extends GebSpec {
+//
+//    def 'welcome page displays welcome message'() {
+//        when:
+//        go '/'
+//
+//        then: 'Static welcome displayed properly'
+//        $('h1').first().text() == 'Hello World'
+//
+//        and: 'Angular generated test displayed properly'
+//        $('h2').first().text() == 'Hello Stranger'
+//    }
+//
+//}
